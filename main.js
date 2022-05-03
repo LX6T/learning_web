f0e9a49d5a0460a914eb9b741d9e2b1e99a65ae4
alex <alex@pullen.net.nz>
2022-05-03 16:17:54 +1200

    added scroll to top script

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-/*
-Comment
- */
+let btt = document.getElementById("back-to-top"),
+    body = document.body,
+    docElem = document.documentElement,
+    offset = 100,
+    scrollPos, docHeight;
 
-let a = 10;
+// Calculate document height
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
+if (docHeight !== 'undefined') {
+    offset = docHeight / 4;
+}
 
-console.log(a);
+// Add scroll event listener
+window.addEventListener("scroll", function(event) {
+    scrollPos = body.scrollTop || docElem.scrollTop;
+
+    btt.className = (scrollPos > offset) ? "visible" : "";
+});
+
+// Add click event listener
+window.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    docElem.scrollTop = 0;
+})
